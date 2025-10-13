b67e405eef2100235744c88f3a2fe2176d503e77
Aumi0 <128075224+Aumi0@users.noreply.github.com>
2025-10-13 22:55:13 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/goyosi/', // Replace with your actual GitHub repo name
+  base: '/final/', // Replace with your actual GitHub repo name
 })
